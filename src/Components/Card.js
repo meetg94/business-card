8cc908bcce8d12657308b8b0f100b1132ed6c07d
bakedegg <meetguleria94@gmail.com>
2021-12-31 05:49:16 -0800

    v2

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import pic from '../Assets/pic.jpeg'
+import {FaLinkedin} from 'react-icons/fa'
+import { FaTwitterSquare } from 'react-icons/fa'
+import { FaGithubSquare } from 'react-icons/fa'
+import { FaInstagramSquare } from 'react-icons/fa'
 
 function Card() {
     return (
@@ -28,7 +32,10 @@ function Card() {
                 Travel Geek. Entrepreneur</p>
             </div>
             <div className="icons">
-                
+                <FaLinkedin size={45}/>
+                <FaTwitterSquare size={45}/>
+                <FaGithubSquare size={45}/>
+                <FaInstagramSquare size={45}/>
             </div>
         </div>
     )
